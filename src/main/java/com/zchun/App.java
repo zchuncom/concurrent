179eb34b2224e948c56aa8d8f75539d47ab9bfcc
zchuncom <i@zchun.com>
2018-07-02 11:15:01 +0800

    master

--- src/main/java/com/zchun/App.java
+++ src/main/java/com/zchun/App.java
@@ -4,10 +4,11 @@ package com.zchun;
  * Hello world!
  *
  */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
+        //master
         System.out.println( "Hello World!" );
     }
 }
